7721fe0f49ebf896c967ea816c90a628c0ffcffb
trismagp <tris.magp@gmail.com>
2024-02-18 17:44:15 +0100

    add app_name in urls and update detail template

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
+from django.http import Http404
 from django.template import loader
 from .models import Question
 
@@ -17,8 +18,17 @@ def index(request):
     }
     return HttpResponse(template.render(context, request))
 
+# def detail(request, question_id):
+#     try:
+#         question = Question.objects.get(pk=question_id)
+#     except Question.DoesNotExist:
+#         raise Http404("Question does not exist")
+#     return render(request, "polls/detail.html", {"question": question})
+
+
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
 
 
 def results(request, question_id):
